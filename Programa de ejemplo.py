ef57e80fb7dea17d840a51d800b475aaf66a9b41
Aaron <aaronloyo2010@gmail.com>
2023-03-24 11:14:58 -0300

    Cree la opcion de jugar

--- Programa de ejemplo.py
+++ Programa de ejemplo.py
@@ -9,3 +9,14 @@ def inicioPrograma():
     print(f"Con que {Variable} eh?, Es un nombre muy bonito")
     print("Quieres jugar un juego?")
     Eleccion = input("(Y/N): ")
+    if Eleccion == "Y":
+        print("!!Exelente!!")
+        print("A que quieres jugar??")
+        print("""0): Ya no quiero jugar
+        1): Tetris
+        2): Minecraft
+        3): Among us
+        4): Roblox
+        5): Adivina el numero """)
+        Eleccion2 = input("Eleccion(5 es la unica que funciona): ")
+        
