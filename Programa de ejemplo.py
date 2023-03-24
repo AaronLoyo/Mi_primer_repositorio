9d2eaef90e72f7259e5ee48f9f749c6e6e6b8f04
Aaron <aaronloyo2010@gmail.com>
2023-03-24 11:07:51 -0300

    Cree la funcion de iniciar el programa

--- Programa de ejemplo.py
+++ Programa de ejemplo.py
@@ -2,3 +2,10 @@ def Interfaz():
     print("""Bienvenido querido usuario, aqui tiene las opciones
     0): Salir del programa
     1): Comenzar el programa""")
+
+def inicioPrograma():
+    print("Elejiste iniciar el programa :D")
+    Variable = input("Dime cual es tu nombre: ")
+    print(f"Con que {Variable} eh?, Es un nombre muy bonito")
+    print("Quieres jugar un juego?")
+    Eleccion = input("(Y/N): ")
